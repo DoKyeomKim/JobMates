7a518bfd72b6c1f30a2c0edd9cdf1ae376d88cc5
dankkun <kwn1209@naver.com>
2024-05-14 10:33:15 +0900

    mypage

--- src/main/java/com/job/mapper/MypageMapper.java
+++ src/main/java/com/job/mapper/MypageMapper.java
@@ -2,7 +2,16 @@ package com.job.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.job.dto.PersonDto;
+import com.job.dto.UserDto;
+
 @Mapper
 public interface MypageMapper {
+
+	PersonDto getPerson(PersonDto personDto);
+
+	void updatePerson(PersonDto personDto);
+	
+	
 
 }
