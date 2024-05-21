bed84eec1e8a54f3255f27020ac0e1f107e13271
DoKyeomKim <mealn21@naver.com>
2024-05-21 21:47:07 +0900

    수정본

--- src/main/java/com/job/controller/PostingController.java
+++ src/main/java/com/job/controller/PostingController.java
@@ -68,7 +68,6 @@ public class PostingController {
 			return new ModelAndView("redirect:/login");
 		}
 		CompanyDto company = postingMapper.getCompanyByUserIdx(userIdx);
-		System.out.println("==============================="+company);
 		
 		
 		List<PostingDto> posting = postingMapper.getPostListByUserIdx(userIdx);
@@ -97,7 +96,7 @@ public class PostingController {
 			mv.setViewName("redirect:/");
 			return mv;
 		}
-		
+
 
 		// posting_tb 갖고오기
 		PostingDto posting = postingMapper.getPostingByPostingIdx(postingIdx);
@@ -106,6 +105,8 @@ public class PostingController {
 			return mv;
 		}
 		Long userType = user.getUserType();
+		
+		mv.addObject("company", company);
 		mv.addObject("userType", userType);
 		mv.addObject("posting", posting);
 		mv.addObject("skill", skill);
